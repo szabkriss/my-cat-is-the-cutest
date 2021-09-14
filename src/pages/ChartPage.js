395bf39350b073a90231252ab41b7bf0356b0671
Kristof A. Szabo <82156829+szabkriss@users.noreply.github.com>
2021-09-14 09:28:19 +0200

    install quickchart

--- src/pages/ChartPage.js
+++ src/pages/ChartPage.js
@@ -3,6 +3,12 @@ function ChartPage() {
     return (
       <div className="ChartPage">
         <h1>Chart Page</h1>
+        <div
+        style={{
+          backgroundImage: `url(https://quickchart.io/chart?c={type:'bar',data:{labels:['Q1','Q2','Q3','Q4'], datasets:[{label:'Users',data:[50,60,70,180]},{label:'Revenue',data:[100,200,300,400]}]}})`
+        }}>
+
+        </div>
       </div>
     );
   }
